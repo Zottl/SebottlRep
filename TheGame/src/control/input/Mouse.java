e54862249647520f63564d80a01a610199ae7edc
Sebastian Schaeffler <sebastian.schaeffler2992@gmail.com>
2015-07-28 19:18:37 +0200

    added mouse listener

--- TheGame/src/control/input/Mouse.java
+++ TheGame/src/control/input/Mouse.java
@@ -12,10 +12,6 @@ public class Mouse implements MouseListener, MouseMotionListener
     private static int mouseX = -1;
     private static int mouseY = -1;
     private static int mouseB = -1;
-    
-    public void mouseClicked(MouseEvent e)
-    {
-    }
 
     public void mousePressed(MouseEvent e)
     {
@@ -34,33 +30,47 @@ public class Mouse implements MouseListener, MouseMotionListener
         mouseX = e.getX();
         mouseY = e.getY();
     }
-    
-    public static int getX ()
+
+    /**
+     * @return x coordinate of the mouse
+     */
+    public static int getX()
     {
         return mouseX;
     }
-    
-    public static int getY ()
+
+    /**
+     * @return y coordinate of the mouse
+     */
+    public static int getY()
     {
         return mouseY;
     }
-    
-    public static int getButton ()
+
+    /**
+     * @return pressed mouse button
+     */
+    public static int getButton()
     {
         return mouseB;
     }
-    
+
     public void mouseDragged(MouseEvent e)
     {
-        
+
     }
-    
+
     public void mouseEntered(MouseEvent e)
     {
-        
+
     }
 
     public void mouseExited(MouseEvent e)
+    {
+
+    }
+    
+    public void mouseClicked(MouseEvent e)
     {
         
     }
