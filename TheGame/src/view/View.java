27a22e9c43eded946b3cd9f5bea5fd81c1a826f4
Sebastian Schaeffler <sebastian.schaeffler2992@gmail.com>
2016-03-17 18:42:56 +0100

    increased window size

--- TheGame/src/view/View.java
+++ TheGame/src/view/View.java
@@ -20,7 +20,7 @@ import javax.swing.SwingUtilities;
 public class View
 {
     // window size variables
-    public static final int WIDTH = 300;
+    public static final int WIDTH = 400;
     public static final int HEIGHT = WIDTH / 16 * 9;
     public static final int SCALE = 3;
 
